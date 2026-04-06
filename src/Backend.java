58f260208964d0dcee48fb2a8936fc6d74367101
koino2 <anchithrajeev@gmail.com>
2026-04-06 20:01:09 +0530

    mvp ready

--- src/Backend.java
+++ src/Backend.java
@@ -7,20 +7,21 @@ public class Backend {
     public Network network;
     public int inputSize = 32*32;
     public int outputSize = 10;
-    public float learningRate = 0.05f;
+    public float learningRate = 0.0001f;
 
     public float[][] trainingSamples;
     public float[][] expectedOutputs;
 
-    public int[] networkSizes = new int[]{inputSize, 100, outputSize};
+    public int[] networkSizes = new int[]{inputSize, 256, outputSize};
     public Backend(){
-        network = CytonAI.networkBuilder(networkSizes, -1, 1);
+        network = CytonAI.networkBuilder(networkSizes, -1f, 1f);
         network.compute();
     }
 
     public void train(){
         for (int i = 0; i < trainingSamples.length; i++) {
             network.inputLayer().setActivations(trainingSamples[i]);
+            network.compute();
             network.backpropagate(expectedOutputs[i], learningRate, true);
         }
     }
@@ -29,5 +30,6 @@ public class Backend {
         network.inputLayer().setActivations(pc.getPixels());
         network.compute();
         System.out.println(Arrays.toString(network.outputLayer().activations()));
+        System.out.println(network.outputLayer().getBrightestNeuronIndex());
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -53,29 +53,36 @@ public class Main {
         File trainingFolder = new File("src/training");
         File[] itemFolders = trainingFolder.listFiles();
 
-        for (int item = 0; item < itemFolders.length; item++) {
-            File itemFolder = new File("src/training/"+getNumberName(item));
-            File[] examples = itemFolder.listFiles();
+        System.out.println("Starting training...");
+        for (int epochs = 0; epochs < 2000; epochs++) {
+            for (int item = 0; item < itemFolders.length; item++) {
+                File itemFolder = new File("src/training/"+getNumberName(item));
+                File[] examples = itemFolder.listFiles();
 
-            backend.trainingSamples = new float[examples.length][pixelCanvas.defaultRes.width*pixelCanvas.defaultRes.height];
-            backend.expectedOutputs = new float[examples.length][10];
-            for (int i = 0; i < examples.length; i++) {
-                BufferedImage img = ImageIO.read(examples[i]);
+                backend.trainingSamples = new float[examples.length][pixelCanvas.defaultRes.width*pixelCanvas.defaultRes.height];
+                backend.expectedOutputs = new float[examples.length][10];
+                for (int i = 0; i < examples.length; i++) {
+                    BufferedImage img = ImageIO.read(examples[i]);
 
-                for (int y = 0; y < img.getHeight(); y++) {
-                    for (int x = 0; x < img.getWidth(); x++) {
-                        pixelCanvas.pixels[y][x] = new Color(img.getRGB(x,y));
+                    float[] pixelArray = new float[pixelCanvas.defaultRes.width * pixelCanvas.defaultRes.height];
+                    for (int y = 0; y < img.getHeight(); y++) {
+                        for (int x = 0; x < img.getWidth(); x++) {
+                            pixelArray[(y*pixelCanvas.canvasSize.width)+x] = new Color(img.getRGB(x,y)).getRed()/255.0f;
+                        }
                     }
+
+                    backend.trainingSamples[i] = pixelArray;
+                    float[] label = new float[10];
+                    label[item] = 1f;
+
+                    backend.expectedOutputs[i] = label;
                 }
-
-                backend.trainingSamples[i] = pixelCanvas.getPixels();
-                float[] label = new float[10];
-                label[item] = 1f;
-
-                backend.expectedOutputs[i] = label;
+                backend.train();
             }
-            backend.train();
+            System.out.printf("\rEpoch "+epochs+" complete.");
         }
+
+        System.out.printf("TRAINING DONE");
 
     }
 
